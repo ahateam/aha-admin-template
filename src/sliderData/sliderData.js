fbfc681674cecc7b8b4d6d08486da430b4828b6c
1403029829@qq.com <1403029829@qq.com>
2018-08-29 19:23:04 +0800

    08/29

---
+++ src/sliderData/sliderData.js
@@ -0,0 +1,62 @@
+
+const sliderData = [
+  {
+    path: '/',
+    name: '系统首页',
+    icon: 'el-icon-time',
+  },
+  {
+    path: '/list',
+    name: '导航天梯',
+    icon: 'el-icon-time',
+    children: [
+      {
+        path: 'list',
+        name: '导航栏一',
+      },
+      {
+        path: 'dist1',
+        name: '导航栏二',
+      },
+      {
+        path: 'dist2',
+        name: '导航栏三',
+      },
+      {
+        path: 'dist3',
+        name: '导航栏四',
+      },
+      {
+        path: 'dist4',
+        name: '导航栏五',
+      }
+    ]
+  },
+  {
+    path: '/set',
+    name: '系统设置',
+    icon: 'el-icon-time',
+    children: [
+      {
+        path: 'set',
+        name: '系统设置',
+      },
+      {
+        path: 'loginSet',
+        name: '登陆设置'
+      },
+      {
+        path: 'userSet',
+        name: '用户设置'
+      }
+    ]
+  }
+];
+
+export default {
+    sliderData: function () {
+      return sliderData;
+    }
+}
+
+
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,22 +1,135 @@
 <template>
-  <el-container>
+  <div id="box">
+    <el-container  id="hebox">
+      <el-aside width="200px" style="background: #545c64" >
+        <el-col :span="24" style="overflow: hidden">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b" style="border: none">
+            <el-menu-item index="1">
+              <i class="el-icon-info"></i>
+              <span slot="title">小红椒系统首页</span>
+            </el-menu-item>
 
-    <el-header>Header</el-header>
-    <el-container>
-      <div v-if="device==='mobile'">xxx</div>
+            <el-submenu v-if="item.children.length > 1" v-for="(item,index) in sliderData" index="72">
 
-      <el-aside width="200px">Aside</el-aside>
-      <el-main>Main</el-main>
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>{{item}}</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="17-1">选项1</el-menu-item>
+                <el-menu-item index="71-2">选项2</el-menu-item>
+                <el-menu-item index="717-3">选项1</el-menu-item>
+                <el-menu-item index="71-4">选项2</el-menu-item>
+                <el-menu-item index="177-5">选项1</el-menu-item>
+                <el-menu-item index="17-6">选项2</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航天梯</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+                <el-menu-item index="1-3">选项1</el-menu-item>
+                <el-menu-item index="1-4">选项2</el-menu-item>
+                <el-menu-item index="1-5">选项1</el-menu-item>
+                <el-menu-item index="1-6">选项2</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-setting"></i>
+                <span>导航天梯</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+                <el-menu-item index="3-3">选项1</el-menu-item>
+                <el-menu-item index="3-4">选项2</el-menu-item>
+                <el-menu-item index="3-5">选项1</el-menu-item>
+                <el-menu-item index="3-6">选项2</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-aside>
+      <el-container>
+        <el-header style="background: #fff">
+          <div style="float: left">
+            <i class="el-icon-upload"></i>
+          </div>
+          <el-breadcrumb style="line-height: 60px;float: left;margin-left: 20px;color:#303133;" separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item><span style="color: #97a8be">活动管理</span></el-breadcrumb-item>
+            <el-breadcrumb-item > <span style="color: #97a8be">活动列表</span></el-breadcrumb-item>
+            <el-breadcrumb-item ><span style="color: #97a8be">活动详情</span></el-breadcrumb-item>
+          </el-breadcrumb>
+        </el-header>
+        <el-main>Main</el-main>
+      </el-container>
     </el-container>
-  </el-container>
+
+
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Main'
-  }
-</script>
+  import sliderData from '@/sliderData/sliderData';
 
+  export default {
+    name: 'Main',
+    methods:{
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    data() {
+      return {
+        clientWidth: '',
+        clientHeight: '',
+        device: '',
+        hebox: '',
+        sliderData:sliderData.sliderData
+      }
+    },
+    mounted() {
+
+      //窗口大小监听器--start
+      // 窗口大小初始值
+      this.clientWidth = document.body.clientWidth;
+      this.clientHeight = document.body.clientHeight;
+      document.getElementById('hebox').style.height=this.clientHeight+'px';
+      // 监听window的resize事件，尺寸变化时更新变量的值
+      const that = this;
+      window.onresize = function windowResize() {
+        that.clientWidth = document.body.clientWidth;
+        that.clientHeight = document.body.clientHeight;
+        document.getElementById('hebox').style.height=that.clientHeight+'px';
+      };
+      //窗口大小监听器--end
+
+    }
+  }
+
+</script>
 <style scoped>
   .el-header, .el-footer {
     background-color: #B3C0D1;
@@ -51,4 +164,5 @@
   .el-container:nth-child(7) .el-aside {
     line-height: 320px;
   }
+
 </style>
