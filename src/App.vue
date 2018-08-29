96802adad0acaa2292941f8097ea85d4e246f124
zhangyu <zhangyu@topoints.cn>
2018-08-29 13:47:15 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,31 @@
 </template>
 
 <script>
+
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        clientWidth,
+        clientHeight,
+      }
+    },
+    mounted() {
+
+      //窗口大小监听器--start
+      // 窗口大小初始值
+      this.clientWidth = document.body.clientWidth;
+      this.clientHeight = document.body.clientHeight;
+      // 监听window的resize事件，尺寸变化时更新变量的值
+      const that = this;
+      window.onresize = function windowResize() {
+        that.clientWidth = document.body.clientWidth;
+        that.clientHeight = document.body.clientHeight;
+        console.error(that.clientWidth + " & " + that.clientHeight);
+      };
+      //窗口大小监听器--end
+
+    }
   }
 </script>
 
