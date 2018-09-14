14594bb45755101c157e559fc372bbb65ce4eca7
1403029829@qq.com <1403029829@qq.com>
2018-09-14 17:28:08 +0800

    两个页面 新增了部分组件介绍

--- src/views/components/btnComponents.vue
+++ src/views/components/btnComponents.vue
@@ -1,6 +1,106 @@
 <template>
     <div>
-      按钮组
+      <div class="box-1">
+        <h2>默认按钮</h2>
+        <el-row>
+          <el-button>默认按钮</el-button>
+          <el-button type="primary">主要按钮</el-button>
+          <el-button type="success">成功按钮</el-button>
+          <el-button type="info">信息按钮</el-button>
+          <el-button type="warning">警告按钮</el-button>
+          <el-button type="danger">危险按钮</el-button>
+        </el-row>
+        <el-row class="button">
+          <el-button disabled>默认按钮</el-button>
+          <el-button type="primary" disabled>主要按钮</el-button>
+          <el-button type="success" disabled>成功按钮</el-button>
+          <el-button type="info" disabled>信息按钮</el-button>
+          <el-button type="warning" disabled>警告按钮</el-button>
+          <el-button type="danger" disabled>危险按钮</el-button>
+        </el-row>
+
+        <el-row class="button">
+          <el-button round>圆角按钮</el-button>
+          <el-button type="primary" round>主要按钮</el-button>
+          <el-button type="success" round>成功按钮</el-button>
+          <el-button type="info" round>信息按钮</el-button>
+          <el-button type="warning" round>警告按钮</el-button>
+          <el-button type="danger" round>危险按钮</el-button>
+        </el-row>
+
+        <el-row class="button">
+          <el-button plain>朴素按钮</el-button>
+          <el-button type="primary" plain>主要按钮</el-button>
+          <el-button type="success" plain>成功按钮</el-button>
+          <el-button type="info" plain>信息按钮</el-button>
+          <el-button type="warning" plain>警告按钮</el-button>
+          <el-button type="danger" plain>危险按钮</el-button>
+        </el-row>
+        <el-row class="button" style="padding-bottom: 34px">
+          <el-button plain disabled>朴素按钮</el-button>
+          <el-button type="primary" plain disabled>主要按钮</el-button>
+          <el-button type="success" plain disabled>成功按钮</el-button>
+          <el-button type="info" plain disabled>信息按钮</el-button>
+          <el-button type="warning" plain disabled>警告按钮</el-button>
+          <el-button type="danger" plain disabled>危险按钮</el-button>
+        </el-row>
+      </div>
+
+      <div class="box-2">
+        <h2>样式按钮</h2>
+        <el-row>
+          <el-button type="primary" :loading="true">加载中</el-button>
+        </el-row>
+
+        <el-row class="button">
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-edit"></el-button>
+            <el-button type="primary" icon="el-icon-share"></el-button>
+            <el-button type="primary" icon="el-icon-delete"></el-button>
+          </el-button-group>
+        </el-row>
+
+        <el-row class="button">
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-arrow-left">上一页</el-button>
+            <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+          </el-button-group>
+        </el-row>
+
+        <el-row class="button">
+          <el-button icon="el-icon-search" circle></el-button>
+          <el-button type="primary" icon="el-icon-edit" circle></el-button>
+          <el-button type="success" icon="el-icon-check" circle></el-button>
+          <el-button type="info" icon="el-icon-message" circle></el-button>
+          <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle></el-button>
+        </el-row>
+
+        <el-row class="button" style="padding-bottom: 30px">
+          <el-button type="primary" icon="el-icon-edit"></el-button>
+          <el-button type="primary" icon="el-icon-share"></el-button>
+          <el-button type="primary" icon="el-icon-delete"></el-button>
+          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+          <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+        </el-row>
+      </div>
+
+      <div class="box-3">
+        <h2>不同尺寸按钮</h2>
+        <el-row>
+          <el-button>默认按钮</el-button>
+          <el-button size="medium">中等按钮</el-button>
+          <el-button size="small">小型按钮</el-button>
+          <el-button size="mini">超小按钮</el-button>
+        </el-row>
+        <el-row class="button" style="padding-bottom: 30px">
+          <el-button round>默认按钮</el-button>
+          <el-button size="medium" round>中等按钮</el-button>
+          <el-button size="small" round>小型按钮</el-button>
+          <el-button size="mini" round>超小按钮</el-button>
+        </el-row>
+      </div>
+
     </div>
 </template>
 
@@ -11,5 +111,26 @@
 </script>
 
 <style scoped>
-
+  .box-1{
+    position: relative;
+    width: 60%;
+    float: left;
+    text-align: center;
+    background: white;
+  }
+  .box-2{
+    position: relative;
+    width: 40%;
+    float: right;
+    text-align: center;
+    background: white;
+  }
+  .button{
+    margin-top: 30px;
+  }
+  .box-3{
+    width: 100%;
+    text-align: center;
+    background: white;
+  }
 </style>
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,13 +1,76 @@
 <template>
-    
+      <div>
+        <el-row class="row-first">
+          <content-to-title :title="title"></content-to-title>
+          <el-row :gutter="20">
+            <el-col :span="12">
+              <div class="img-box">
+                <img src="@/assets/image/layout1.png" alt="">
+              </div>
+            </el-col>
+            <el-col :span="12"  >
+              <div class="img-box">
+                <img src="@/assets/image/layout2.png" alt="">
+              </div>
+            </el-col>
+          </el-row>
+
+          <el-row style="margin-top: 20px">
+            <content-to-title :title="title1"></content-to-title>
+         </el-row>
+          <el-row :gutter="20" >
+            <el-col :span="12">
+              <div class="img-box">
+                <img src="@/assets/image/layout3.png" alt="">
+              </div>
+            </el-col>
+            <el-col :span="12">
+
+            </el-col>
+          </el-row>
+        </el-row>
+      </div>
 </template>
 
 <script>
+  import ContentToTitle from '@/components/ContentToTitle'
     export default {
-        name: "layout"
+        name: "layout",
+        data(){
+            return{
+              title:'当前项目布局',
+              title1:'可使用布局',
+              html1:``
+            }
+        },
+        components:{
+          ContentToTitle
+        }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .row-first {
+    background: #fff;
+    padding: $small-padding;
+  }
+  .img-box{
+    width: 100%;
+    overflow: hidden;
+    height: 215px;
+  }
+  .img-box img{
+    width: inherit;
+    height: 215px;
+  }
+  .demo-block .highlight code.hljs {
+    margin: 0;
+    border: none;
+    max-height: none;
+    border-radius: 0;
+  }
+  .hljs {
+    font-size: 12px;
+    background-color: #fafafa;
+  }
 </style>
