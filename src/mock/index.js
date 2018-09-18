359f79e33ac4f4ca50434ef32e4a007168346f9e
1403029829@qq.com <1403029829@qq.com>
2018-09-18 10:21:18 +0800

    完成初步的mock 集成

--- src/mock/index.js
+++ src/mock/index.js
@@ -6,11 +6,11 @@ const Random =Mock.Random;
 const produceData = function (opt) {
   console.log('opt',opt);
   let articles = [];      //组合假数据的数组名
-  for (let i = 0; i < 30 ; i++){ //定义数据生成规则
+  for (let i = 0; i < 5 ; i++){ //定义数据生成规则
     let newArticleObject = {
       title:Random.csentence(5,30),
       thunb_img:Random.dataImage('300*250','mock的图片'),
-      name:Random.cname(),
+      name:'唐诗恶疾',
       date:Random.date()+' '+Random.time(),
       email:Random.email(),
     }
--- src/views/mock/showMock.vue
+++ src/views/mock/showMock.vue
@@ -1,15 +1,63 @@
 <template>
     <div>
+      <el-row class="row-first" >
+        <content-to-title title="mock测试： /user "></content-to-title>
+         <el-row style="margin-top: 20px;border-bottom: 1px solid #ddd;" v-for="(item,_index) in userData" :key="_index">
+             <p>
+               <span>姓名：</span><span>{{item.name}}</span>
+             </p>
+             <p>
+               <span>头像：</span><span>{{item.thunb_img}}</span>
+             </p>
+             <p>
+               <span>邮箱：</span> <span>{{item.email}}</span>
+             </p>
 
+         </el-row>
+      </el-row>
     </div>
 </template>
 
 <script>
+  import ContentToTitle from '@/components/ContentToTitle'
+
     export default {
-        name: "showMock"
+        name: "showMock",
+      components:{
+        ContentToTitle
+      },
+      data(){
+          return{
+            userData:[]
+          }
+      },
+      mounted(){
+          this.getUserData()
+      },
+      methods:{
+          getUserData(){
+            console.log('开始请求');
+            let that = this;
+            this.$http.get('/user')
+              .then(function (res) {
+                console.log('请求成功了');
+                that.userData = res.data.data;
+              })
+              .catch(function (err) {
+                console.log('错误连接'+err);
+              })
+          }
+      }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .row-first {
+    background: #fff;
+    padding: $small-padding;
+    margin: 10px;
+  }
+  p{
+    padding: 0;
+  }
 </style>
